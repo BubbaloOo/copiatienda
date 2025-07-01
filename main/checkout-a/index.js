16628d7759e316bad926d7e5b55825e6a1a94420
BubbaloOo <147291164+BubbaloOo@users.noreply.github.com>
2025-07-01 07:53:24 -0600

    Delete main directory

--- main/checkout-a/index.js
+++
@@ -1,3 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-  const producto = params.get("producto");
-  document.getElementById("producto-nombre").textContent = producto || "Producto no especificado";
