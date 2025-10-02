c0953522ce1f2c8220c3f4a101102e52b5144164
BubbaloOo <147291164+BubbaloOo@users.noreply.github.com>
2025-10-02 17:52:20 +0200

    Delete main directory

--- main/categories/categories.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    // --- Lógica del Menú Hamburguesa para Móvil ---
-    const hamburger = document.getElementById('hamburger-menu');
-    const nav = document.getElementById('main-nav');
-
-    hamburger.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });});
